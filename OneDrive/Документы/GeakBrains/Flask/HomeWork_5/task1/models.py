3eea420a6deddb046becd5aecc7cdb4c45387048
Eduard <dirol2016@list.ru>
2023-07-17 20:10:30 +0500

    HW_5

---
+++ OneDrive/Документы/GeakBrains/Flask/HomeWork_5/task1/models.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+
+class UserBase(BaseModel):
+    name: str
+    email: EmailStr
+
+
+class UserIn(UserBase):
+    password: str
+
+
+class UserOut(UserBase):
+    id: int
+
+
+class UserPut(BaseModel):
+    name: Optional[str]
+    email: Optional[EmailStr]
+    password: Optional[str]
---
+++ OneDrive/Документы/GeakBrains/Flask/HomeWork_5/task2/models.py
@@ -0,0 +1,24 @@
+from enum import StrEnum
+from typing import Optional
+
+
+class TaskStatus(StrEnum):
+    TO_DO = 'todo'
+    IN_PROCESS = 'in process'
+    DONE = 'done'
+
+
+class Task(BaseModel):
+    title: str
+    description: Optional[str]
+    status: TaskStatus = TaskStatus.TO_DO
+
+
+class TaskOut(Task):
+    id: int
+
+
+class TaskPut(BaseModel):
+    title: Optional[str]
+    description: Optional[str]
+    status: Optional[TaskStatus]
